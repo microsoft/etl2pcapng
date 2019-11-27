79456d361ff721e888dfec74cbcb3c4d93ce7ea1
Matt Olson <maolson@ntdev.microsoft.com>
2019-11-27 15:52:30 -0800

    update package script

--- src/make_release.cmd
+++ src/make_release.cmd
@@ -1,12 +1,13 @@
 @echo off
 msbuild -t:rebuild -p:configuration=release -p:platform=win32
 msbuild -t:rebuild -p:configuration=release -p:platform=x64
-rmdir /s /q releases
-mkdir releases
-mkdir releases\x64
-mkdir releases\x86
-copy x64\release\etl2pcapng.exe releases\x64
-copy x64\release\etl2pcapng.pdb releases\x64
-copy release\etl2pcapng.exe releases\x86
-copy release\etl2pcapng.pdb releases\x86
-echo Now zip up releases directory and upload to github.
+rmdir /s /q etl2pcapng
+del etl2pcapng.zip
+mkdir etl2pcapng
+mkdir etl2pcapng\x64
+mkdir etl2pcapng\x86
+copy x64\release\etl2pcapng.exe etl2pcapng\x64
+copy x64\release\etl2pcapng.pdb etl2pcapng\x64
+copy release\etl2pcapng.exe etl2pcapng\x86
+copy release\etl2pcapng.pdb etl2pcapng\x86
+echo Now zip up etl2pcapng directory and upload to github.
