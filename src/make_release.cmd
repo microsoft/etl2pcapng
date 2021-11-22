5be5cdae114f95877e7d478ec363ddc337535753
Thad House <thhous@microsoft.com>
2021-11-22 12:21:26 -0800

    Switch build to be CMake based

--- src/make_release.cmd
+++ src/make_release.cmd
@@ -1,13 +1,25 @@
 @echo off
-msbuild -t:rebuild -p:configuration=release -p:platform=win32
-msbuild -t:rebuild -p:configuration=release -p:platform=x64
+pushd %~dp0
+rmdir /s /q build_release
+mkdir build_release
+cd build_release
+mkdir x64
+mkdir x86
+cd x64
+cmake -A x64 ../..
+cmake --build . --config Release
+cd ..
+cd x86
+cmake -A Win32 ../..
+cmake --build . --config Release
+popd
 rmdir /s /q etl2pcapng
 del etl2pcapng.zip
 mkdir etl2pcapng
 mkdir etl2pcapng\x64
 mkdir etl2pcapng\x86
-copy x64\release\etl2pcapng.exe etl2pcapng\x64
-copy x64\release\etl2pcapng.pdb etl2pcapng\x64
-copy release\etl2pcapng.exe etl2pcapng\x86
-copy release\etl2pcapng.pdb etl2pcapng\x86
+copy build_release\x64\Release\etl2pcapng.exe etl2pcapng\x64
+copy build_release\x64\Release\etl2pcapng.pdb etl2pcapng\x64
+copy build_release\x86\Release\etl2pcapng.exe etl2pcapng\x86
+copy build_release\x86\Release\etl2pcapng.pdb etl2pcapng\x86
 echo Now zip up etl2pcapng directory and upload to github.
