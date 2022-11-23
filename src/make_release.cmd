f0153c8e7c0e7906a8094872ff1c148ae66ce12b
Nick Banks <nibanks@microsoft.com>
2022-11-23 08:22:04 -0500

    Move things around and only publish .msi

--- src/make_release.cmd
+++ src/make_release.cmd
@@ -10,6 +10,8 @@ mkdir build_release\x86
 cd build_release\x64
 cmake -A x64 ../..
 cmake --build . --config Release
+"C:/Program Files (x86)/WiX Toolset v3.11/bin/candle.exe" ..\..\installer.wxs -o etl2pcapng.wixobj
+"C:/Program Files (x86)/WiX Toolset v3.11/bin/light.exe" -b .\ -o etl2pcapng.msi etl2pcapng.wixobj
 cd ..\x86
 cmake -A Win32 ../..
 cmake --build . --config Release
@@ -21,11 +23,9 @@ mkdir etl2pcapng\x64
 mkdir etl2pcapng\x86
 copy build_release\x64\Release\etl2pcapng.exe etl2pcapng\x64
 copy build_release\x64\Release\etl2pcapng.pdb etl2pcapng\x64
+copy build_release\x64\Release\etl2pcapng.msi etl2pcapng\x64
 copy build_release\x86\Release\etl2pcapng.exe etl2pcapng\x86
 copy build_release\x86\Release\etl2pcapng.pdb etl2pcapng\x86
-
-"C:/Program Files (x86)/WiX Toolset v3.11/bin/candle.exe" installer.wxs -o etl2pcapng\x64\etl2pcapng.wixobj
-"C:/Program Files (x86)/WiX Toolset v3.11/bin/light.exe" -b etl2pcapng\x64 -o etl2pcapng\x64\etl2pcapng.msi etl2pcapng\x64\etl2pcapng.wixobj
 
 if exist etl2pcapng.zip del etl2pcapng.zip
 echo Now zip up etl2pcapng directory and upload to github.
