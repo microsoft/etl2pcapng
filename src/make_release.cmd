4db7d38b2e7936a6cdb13e2ed6da88efcf4c60f5
Nick Banks <nibanks@microsoft.com>
2022-11-23 08:12:49 -0500

    Putin make release

--- src/make_release.cmd
+++ src/make_release.cmd
@@ -24,6 +24,9 @@ copy build_release\x64\Release\etl2pcapng.pdb etl2pcapng\x64
 copy build_release\x86\Release\etl2pcapng.exe etl2pcapng\x86
 copy build_release\x86\Release\etl2pcapng.pdb etl2pcapng\x86
 
+"C:/Program Files (x86)/WiX Toolset v3.11/bin/candle.exe" installer.wxs -o etl2pcapng\x64\etl2pcapng.wixobj
+"C:/Program Files (x86)/WiX Toolset v3.11/bin/light.exe" -b etl2pcapng\x64 -o etl2pcapng.msi etl2pcapng\x64\etl2pcapng.wixobj
+
 if exist etl2pcapng.zip del etl2pcapng.zip
 echo Now zip up etl2pcapng directory and upload to github.
 
